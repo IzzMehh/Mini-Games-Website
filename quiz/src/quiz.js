8a26849996bce501d499948ec8d5a44129535acb
GauravV <104690376+IzzMehh@users.noreply.github.com>
2024-05-26 09:57:45 +0000

    Added About page for quizz and added game icon in quiz mobile view

--- quiz/src/quiz.js
+++ quiz/src/quiz.js
@@ -4,3 +4,14 @@ import { playImgEvent } from "./begin.js";
 export let quizDataStored = accountStoredData
 
 playImgEvent()
+
+
+const startBtn = document.querySelector('#start-btn')
+
+const aboutPage = document.querySelector('#about-quiz')
+const section2 = document.querySelector('#section-2')
+
+startBtn.addEventListener('click',()=>{
+    aboutPage.style.display = 'none'
+    section2.style.display = 'block'
+})
